9aad32d62b77aef44aa0945163fe0b264248a303
alissoninacio1 <alissoninacio96@gmail.com>
2023-09-24 16:11:57 -0700

    updated contacts file

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,23 +1,3 @@
-// const connectToMongo = require("../database/mongo");
-
-// async function getContacts() {
-//   const { client, contactsCollection } = await connectToMongo();
-
-//   try {
-//     const contacts = await contactsCollection.find({}).toArray();
-//     return contacts;
-//   } catch (err) {
-//     console.error("Error:", err);
-//   } finally {
-//     client.close();
-//     console.log("Disconnected from MongoDB");
-//   }
-// }
-
-// module.exports = {
-//   getContacts
-// };
-
 const connectToMongo = require("../database/mongo");
 
 async function queryCollection(queryFunction) {
