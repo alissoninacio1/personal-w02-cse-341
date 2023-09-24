6d6ae27a71f1fb62561b52583158596c0e65fa3b
alissoninacio1 <alissoninacio96@gmail.com>
2023-09-23 21:53:56 -0700

    code refatored

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const contactsRouter = require("./routes/index");
 
-app.use("/", contactsRouter);
-
+app.use("/", require("./routes/index"));
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}!`);
