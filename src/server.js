0f6d652a013aa5ceabfaab171ac9cf8395b1f368
alissoninacio1 <alissoninacio96@gmail.com>
2023-10-05 17:53:51 -0700

    swagger configured

--- src/server.js
+++ src/server.js
@@ -1,20 +1,40 @@
+// const express = require('express');
+// const app = express();
+// const port = 8080;
+
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerSpec = require('./swagger'); 
+
+// const contactsRouter = require('./routes/index');
+
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// app.use(express.json());
+
+// app.use('/', contactsRouter);
+// app.use('/contacts', contactsRouter);
+
+// app.listen(process.env.PORT || port, () => {
+//   console.log(`Example app listening on port ${port}!`);
+// });
+
+
 const express = require('express');
 const app = express();
 const port = 8080;
-
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swagger'); 
+const swaggerDocument = require('./swagger_output.json'); 
 
 const contactsRouter = require('./routes/index');
 
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 
 app.use('/', contactsRouter);
 app.use('/contacts', contactsRouter);
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening on port ${port}!`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
--- src/swagger.js
+++ src/swagger.js
@@ -1,16 +1,33 @@
-const swaggerJsdoc = require('swagger-jsdoc');
+// const swaggerJsdoc = require('swagger-jsdoc');
 
-const options = {
-  swaggerDefinition: {
-    info: {
-      title: 'Contacts API',
-      version: '1.0.0',
-      description: 'Documentation for the Contacts API',
-    },
+// const options = {
+//   swaggerDefinition: {
+//     info: {
+//       title: 'Contacts API',
+//       version: '1.0.0',
+//       description: 'Documentation for the Contacts API',
+//     },
+//   },
+//   apis: ['./routes/index.js'], 
+// };
+
+// const swaggerSpec = swaggerJsdoc(options);
+
+// module.exports = swaggerSpec;
+
+
+
+const swaggerAutogen = require('swagger-autogen')();
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./routes/index.js']; 
+
+const doc = {
+  info: {
+    title: 'API Documentation',
+    description: 'Documentation for your API',
   },
-  apis: ['./routes/index.js'], 
+  host: 'localhost:8080', 
+  basePath: '/',
 };
 
-const swaggerSpec = swaggerJsdoc(options);
-
-module.exports = swaggerSpec;
+swaggerAutogen(outputFile, endpointsFiles, doc);
