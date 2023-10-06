eabc98a2d00435b26bf7d72c47ec7b15054efc6f
alissoninacio1 <alissoninacio96@gmail.com>
2023-10-05 23:05:49 -0700

    comment removed

--- src/server.js
+++ src/server.js
@@ -1,24 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const port = 8080;
-
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerSpec = require('./swagger'); 
-
-// const contactsRouter = require('./routes/index');
-
-
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// app.use(express.json());
-
-// app.use('/', contactsRouter);
-// app.use('/contacts', contactsRouter);
-
-// app.listen(process.env.PORT || port, () => {
-//   console.log(`Example app listening on port ${port}!`);
-// });
-
 
 const express = require('express');
 const app = express();
